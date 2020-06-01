6b775b6f9f563d4664a007d525f2917e30678b34
duc <hohuynhduca7bh123@gmail.com>
2020-06-01 10:50:10 +0700

    new project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCXogq0ovWmMOT-Efv0qCd7_eSIqMlCWHY",
+    authDomain: "itblog-a3e53.firebaseapp.com",
+    databaseURL: "https://itblog-a3e53.firebaseio.com",
+    projectId: "itblog-a3e53",
+    storageBucket: "itblog-a3e53.appspot.com",
+    messagingSenderId: "804850134728",
+    appId: "1:804850134728:web:6dd27610897bbb350d7ffd",
+    measurementId: "G-P4FVRX0G7L"
+  }
 };
 
 /*
